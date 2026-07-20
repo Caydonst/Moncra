d16a73f3851abf719902a748c99b25f1a166a3ad
Caydon Thomas <caydons.thomas@gmail.com>
2026-07-20 17:13:00 -0400

    Working on getting server running

--- server/src/auth/verifySupabaseToken.ts
+++ server/src/auth/verifySupabaseToken.ts
@@ -6,11 +6,10 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey =
     process.env.SUPABASE_PUBLISHABLE_KEY ??
     process.env.SUPABASE_ANON_KEY;
-/*
+
 if (!supabaseUrl) {
     throw new Error("Missing SUPABASE_URL");
 }
-    */
 
 if (!supabaseKey) {
     throw new Error(
